b63ab7ded95b1ce15e0c01c520aa3c4c76dee606
mystborn <thefirewithin1017@gmail.com>
2019-05-19 19:20:13 -0700

    Clarified ecs_messages.c initialization order

--- src/ecs_messages.c
+++ src/ecs_messages.c
@@ -7,6 +7,7 @@ EcsEventManager* ecs_entity_disabled;
 EcsEvent* ecs_world_disposed;
 
 void ecs_messages_init(void) {
+    // This has to be initialized first, because EcsEventManagers add a function to it.
     ecs_world_disposed = ecs_event_init();
 
     ecs_entity_created = ecs_event_define();
